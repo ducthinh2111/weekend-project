c741b57fafbae7c4cde62568662566d912e67057
Thinh Le <thinhle2199@gmail.com>
2024-04-14 14:50:25 +0700

    initial commit

---
+++ solve-problem/java-wproj/src/main/java/com/starea/AOCDayOne.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class AOCDayOne {
+    public static void main(String[] args) {
+        String filename = "AOCDayOne.txt";
+        try (Scanner scanner = new Scanner(Objects.requireNonNull(AOCDayOne.class.getClassLoader().getResourceAsStream(filename)))) {
+            int sum = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                int extractedNumber = extractNumber(line);
+                sum += extractedNumber;
+            }
+            System.out.println(sum);
+        }
+    }
+
+    private static int extractNumber(String line) {
+        String removedCharacters = line.replaceAll("[a-z|A-Z]", "");
+
+        if (removedCharacters.isEmpty()) {
+            return 0;
+        }
+        int numFirst = Integer.parseInt(String.valueOf(removedCharacters.charAt(0)));
+        if (removedCharacters.length() == 1) {
+            return numFirst * 10 + numFirst;
+        } else {
+            int numLast = Integer.parseInt(String.valueOf(removedCharacters.charAt(removedCharacters.length() - 1)));
+            return numFirst * 10 + numLast;
+        }
+    }
+}
