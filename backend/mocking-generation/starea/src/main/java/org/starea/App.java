98ee99d93e104525fc8ba46527097252ff549d06
Thinh Le <thinhle2199@gmail.com>
2024-05-12 10:33:31 +0700

    add parser

--- backend/mocking-generation/starea/src/main/java/org/starea/App.java
+++ backend/mocking-generation/starea/src/main/java/org/starea/App.java
@@ -1,12 +1,24 @@
 package org.starea;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final String FILE_PATH = "src/main/resources/AddressService.txt";
+
+    public static void main(String[] args) throws Exception {
+
+        CompilationUnit cu = StaticJavaParser
+                .parse(Files.newInputStream(Paths.get(FILE_PATH)));
+        VoidVisitor<Void> methodNameVisitor = new MethodNamePrinter();
+        methodNameVisitor.visit(cu, null);
     }
 }
