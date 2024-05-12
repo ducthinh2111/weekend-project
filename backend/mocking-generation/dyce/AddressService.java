90fd5394ca286d6766ec2678274d5595fc065aab
Oz <xad.love@gmail.com>
2024-05-12 11:55:51 +0700

    access class

---
+++ backend/mocking-generation/dyce/AddressService.java
@@ -0,0 +1,22 @@
+public class AddressService {
+
+    @Inject
+    private AddressDao addrDao;
+
+    public Address create(Address address) {
+        return addrDao.create(address);
+    }
+
+    public int delete(Address address) {
+        return addrDao.delete(address);
+    }
+
+    public void update(Address address) {
+        addrDao.update(address);
+    }
+
+    public Address merge(Address addr1, Address addr2) {
+        return addrDao.merge(addr1, addr2);
+    }
+}
+
--- backend/mocking-generation/dyce/App.java
+++ backend/mocking-generation/dyce/App.java
@@ -1,10 +1,32 @@
-import static com.github.javaparser.StaticJavaParser.*;
+import java.io.File;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import static com.github.javaparser.StaticJavaParser.*;
 
 public class App {
     public static void main(String[] args) {
-        CompilationUnit cu = parse("class X{}");
-        System.out.println("Hi there!" + cu);
+        if (args.length < 1)
+            throw new RuntimeException("file name must be provided!");
+        try {
+            new App().gen(args[0]);
+        } catch (Exception e) {
+            throw new RuntimeException("__[xX]: ", e);
+        }
+    }
+
+    public void gen(String file) throws Exception {
+        String className = toClassName(file);
+        System.out.println("class: " + className);
+        CompilationUnit cu = parse(new File(file));
+        System.out.println(cu);
+        System.out.println("---- ---- ---- ---- ---- ---- ---- ");
+        ClassOrInterfaceDeclaration clazz = cu.getClassByName(className).get();
+        System.out.println(clazz.getName());
+    }
+
+    private String toClassName(String filename) {
+        int idx = filename.indexOf('.');
+        return filename.substring(0, idx);
     }
 }
 
