c3bd7322311fa312e3a9c6b8c0b94bbf0d74c22a
Thinh Le <thinhle2199@gmail.com>
2024-06-30 16:48:01 +0700

    maybe solved

--- solve-problem/hacktree/starea/HackTree.java
+++ solve-problem/hacktree/starea/HackTree.java
@@ -1,7 +1,10 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class HackTree {
     private static int countVerticalPaths(List<Integer> cost,
@@ -11,80 +14,72 @@ public class HackTree {
                                           int k) {
 
         Map<Integer, List<Integer>> connection = new HashMap<>();
-
-        // make the root
-        for (int i = 0; i < edgeNodes - 1; i++) {
-            if (Objects.equals(edgeFrom.get(i), 1)) {
-                if (!connection.containsKey(edgeFrom.get(i))) {
-                    connection.put(edgeFrom.get(i), new ArrayList<>());
-                }
-                connection.get(edgeFrom.get(i)).add(edgeTo.get(i));
-            }
-            if (Objects.equals(edgeTo.get(i), 1)) {
-                if (!connection.containsKey(edgeTo.get(i))) {
-                    connection.put(edgeTo.get(i), new ArrayList<>());
-                }
-                connection.get(edgeTo.get(i)).add(edgeFrom.get(i));
-            }
-        }
-
-        // from the root
-        for (int i = 0; i < edgeNodes - 1; i++) {
-            if (connection.get(1).contains(edgeFrom.get(i))) {
-                if (!connection.containsKey(edgeFrom.get(i))) {
-                    connection.put(edgeFrom.get(i), new ArrayList<>());
-                }
-                connection.get(edgeFrom.get(i)).add(edgeTo.get(i));
-            }
-
-            if (connection.get(1).contains(edgeTo.get(i))) {
-                if (!connection.containsKey(edgeTo.get(i))) {
-                    connection.put(edgeTo.get(i), new ArrayList<>());
-                }
-                connection.get(edgeTo.get(i)).add(edgeFrom.get(i));
-            }
-        }
-
-        // Calculate cost
-        int result = 0;
-        for (int i = 2; i <= edgeNodes; i++) {
-            if (cost.get(i - 1).longValue() % k == 0) {
-                result++;
-            }
-        }
-
-        List<Long> previousCost = new ArrayList<>();
-        previousCost.add(cost.getFirst().longValue());
-        for (Integer child : connection.get(1)) {
-            long childCost = cost.get(child - 1).longValue();
-            List<Long> newPreviousCost = previousCost.stream().map(pre -> pre + childCost).collect(Collectors.toList());
-            newPreviousCost.add(childCost);
-            result += calculateCost(connection, connection.get(child), cost, newPreviousCost, k);
-        }
-        return result;
-    }
-
-    private static int calculateCost(Map<Integer, List<Integer>> connection,
-                                     List<Integer> children,
-                                     List<Integer> cost,
-                                     List<Long> previousCost,
-                                     int k) {
-
-        if (children != null) {
-            for (Integer child : children) {
-                long childCost = cost.get(child - 1).longValue();
-                List<Long> newPreviousCost = previousCost.stream().map(pre -> pre + childCost).collect(Collectors.toList());
-                newPreviousCost.add(childCost);
-                return calculateCost(connection, connection.get(child), cost, newPreviousCost, k);
-            }
-        }
+        List<Integer> goneThoughEdgeIndex = new ArrayList<>();
+        List<Long> costPath = new ArrayList<>();
 
         int count = 0;
-        for (Long costPath : previousCost) {
-            if (costPath % k == 0) {
+        for (Integer c : cost) {
+            if (c % k == 0) {
                 count++;
             }
         }
+        return count + traverse(1,
+                edgeFrom, edgeTo, edgeNodes - 1, connection, goneThoughEdgeIndex, cost, costPath, 0, k);
+    }
+
+    private static int traverse(Integer parent,
+                                 List<Integer> edgeFrom,
+                                 List<Integer> edgeTo,
+                                 int edges,
+                                 Map<Integer, List<Integer>> connection,
+                                 List<Integer> goneThoughEdgeIndex,
+                                 List<Integer> costs,
+                                 List<Long> costPath,
+                                int deep,
+                                int k) {
+
+        for (int i = 0; i < edges; i++) {
+            if (goneThoughEdgeIndex.contains(i)) continue;
+
+            if (Objects.equals(edgeFrom.get(i), parent)) {
+                if (!connection.containsKey(edgeFrom.get(i))) {
+                    connection.put(edgeFrom.get(i), new ArrayList<>());
+                }
+                connection.get(edgeFrom.get(i)).add(edgeTo.get(i));
+                goneThoughEdgeIndex.add(i);
+            }
+
+            if (Objects.equals(edgeTo.get(i), parent)) {
+                if (!connection.containsKey(edgeTo.get(i))) {
+                    connection.put(edgeTo.get(i), new ArrayList<>());
+                }
+                connection.get(edgeTo.get(i)).add(edgeFrom.get(i));
+                goneThoughEdgeIndex.add(i);
+            }
+        }
+
+        List<Integer> children = connection.get(parent);
+        if (Objects.isNull(children)) {
+            int count = 0;
+            for (Long calculatedCost : costPath) {
+                if (calculatedCost % k == 0) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        int count = 0;
+        for (Integer child : connection.get(parent)) {
+            long cost = costs.get(child - 1).longValue();
+            List<Long> clonedCostPath = new ArrayList<>(costPath);
+            clonedCostPath.add(cost + costs.get(parent - 1).longValue());
+            for (int i = costPath.size() - deep; i < costPath.size(); i++) {
+                clonedCostPath.add(cost + costPath.get(i));
+            }
+            count += traverse(child, edgeFrom, edgeTo, edges, connection, goneThoughEdgeIndex, costs, clonedCostPath, deep + 1, k);
+        }
+
         return count;
     }
 }
