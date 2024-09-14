e65dd7823bfdfaacd8eb9395ac861081ce69aa99
ngan_ori <nganttkbt@gmail.com>
2024-09-14 09:52:28 +0700

    From ori

---
+++ backend/mocking-generation/ori/src/main/Address.java
@@ -0,0 +1,29 @@
+public class Address {
+    private String houseNumber;
+    private String street;
+    private String city;
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
