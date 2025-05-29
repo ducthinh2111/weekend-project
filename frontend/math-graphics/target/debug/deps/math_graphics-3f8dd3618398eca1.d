8f4e736afcb5c0189a9b92cc1a900667b1ceb62b
Starea <thinhle2199@gmail.com>
2025-05-29 22:02:22 +0700

    remove target

--- frontend/math-graphics/target/debug/deps/math_graphics-3f8dd3618398eca1.d
+++
@@ -1,5 +0,0 @@
-/Users/starea/dev/weekend-project/frontend/math-graphics/target/debug/deps/libmath_graphics-3f8dd3618398eca1.rmeta: src/main.rs
-
-/Users/starea/dev/weekend-project/frontend/math-graphics/target/debug/deps/math_graphics-3f8dd3618398eca1.d: src/main.rs
-
-src/main.rs:
--- frontend/math-graphics/target/debug/deps/math_graphics-6a3272a67560077f.d
+++
@@ -1,5 +0,0 @@
-/Users/starea/dev/weekend-project/frontend/math-graphics/target/debug/deps/libmath_graphics-6a3272a67560077f.rmeta: src/main.rs
-
-/Users/starea/dev/weekend-project/frontend/math-graphics/target/debug/deps/math_graphics-6a3272a67560077f.d: src/main.rs
-
-src/main.rs:
